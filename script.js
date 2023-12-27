2f69f7b918e4b6780799d42bab8d40c144845e38
Your Name <you@example.com>
2023-12-27 09:54:30 +0700

    f1.27.12.2023

---
+++ script.js
@@ -0,0 +1,38 @@
+//Interaction: alert, prompt, confirm
+//prompt
+// var userName  = prompt("Ban ten la gi","ban xinh");
+// alert(`chao ${userName?userName:"ban"}, rat vui duoc gap ban`)
+
+
+// confirm
+// var isOk = confirm("ghe tham trang web cua toi nhe");
+// if(isOk){alert("go to js.info")}
+
+
+
+//_______________________________________________________________
+//Typed COnversions
+//numeric conversion
+// var numericArr = [undefined, null, true, false, "this is string"]
+// numericArr.forEach(e=>console.log(`Number(${e}) = ${Number(e)}`))
+
+
+//_______________________________________________________________
+//Basic operators, maths
+// operand/arguments
+//operator
+
+//Maths
+// + - * / % **
+
+//string concatenation with binary +
+// console.log(`"my"+"string" = "${"my"+"string"}"`)
+//....
+
+//Bitwise operators
+//AND(&)
+//OR(|)
+//XOR(~)
+
+
+// https://javascript.info/operators#terms-unary-binary-operand
